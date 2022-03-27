b90dc5ead860473babe38ff9b6324204861bf63b
Nin <darshan310268@gmail.com>
2022-03-27 18:39:54 +0530

    Update guild.model.js

--- models/guild.model.js
+++ models/guild.model.js
@@ -1,1 +1,33 @@
-// soon™️
+/* eslint-disable no-unused-vars */
+const chalk = require('chalk');
+const { DataTypes } = require('sequelize');
+const Client = require('../structures/client');
+
+class Guild {
+  /**
+   * @param {Client} client
+   */
+  constructor(client) {
+    this.client = client;
+  }
+
+  async init() {
+    const db = this.client.sequel.define('guild', {
+      guild: {
+        type: DataTypes.STRING,
+      },
+      channel: {
+        type: DataTypes.STRING,
+      },
+      msg: {
+        type: DataTypes.STRING,
+      },
+    });
+
+    this.raw = db;
+    await this.raw.sync();
+    console.log(chalk.blueBright(`[<>] Database "Guild" Loaded`));
+  }
+}
+
+module.exports = Guild;
